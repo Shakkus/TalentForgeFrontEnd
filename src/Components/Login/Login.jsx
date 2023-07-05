97e61ca4947e12a45e1ba3d0da076a7af50e5fbd
Sebastian Albornoz <111798782+Shakkus@users.noreply.github.com>
2023-07-05 18:37:40 -0300

    Añadiendo login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,12 +1,39 @@
-import { useEffect, useState } from "react";
-import "./style.css";
-import { useForm } from "react-hook-form";
+import "./Login.css";
 
 const Login = () => {
 
   return (
-    <div className="form">
-      
+    <div className="login">
+        <h1>Account Login</h1>
+        <div className="continueWith">
+            <div className="continueWithGoogle">
+            <p>Continue with</p>
+            <img src="image 109.png" alt="" />
+            </div>
+            <div className="continueWithGmail">
+            <p>Continue with</p>
+            <img src="image 87.png" alt="" />
+            </div>
+
+            <div className="continueWithTwitter">
+            <p>Continue with</p>
+            <img src="image 88.png" alt="" />
+            </div>
+        </div>
+
+        <form action="">
+            <div className="inputName">
+                <label htmlFor="">Email</label>
+                <input type="text" />
+            </div>
+            <div className="inputPassword">
+                <label htmlFor="">Password</label>
+                <input type="text" />
+            </div>
+            <div>
+            <input type="submit" value="Submit" className="buttonSubmit" />
+          </div>
+        </form>
     </div>
   );
 };
