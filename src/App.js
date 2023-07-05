561572c4425ab1ba2768dd4a5d45f992ed99be25
ERehbani <elianleesin@gmail.com>
2023-07-05 20:36:18 -0300

    intento instalar Tailwind

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Landing from "./Components/LandingPage/LandingPage";
 import Form from "./Components/Form/Register";
 import DetailCourses from "./Components/DetailCoursesProgramation/DetailCoursesProgramation";
+import "tailwindcss/tailwind.css";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/landing" element={<Landing />} />
         <Route path="/detail" element={<DetailCourses />} />
       </Routes>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
