0fffd1bfa0f59ab69d67115cedd51bc66d353c7a
Sergiofrfr <serfabian619@gmail.com>
2023-07-06 09:28:02 -0500

    se agrego la carpeta perfil

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Landing from "./Components/LandingPage/LandingPage";
 import Form from "./Components/Form/Register";
+import Profile from "./Components/Profile/Profile";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<Form />} />
         <Route path="/presentation" element={<Landing />} />
+        <Route path="/profile" element={< Profile/>}/>
       </Routes>
     </div>
   );
