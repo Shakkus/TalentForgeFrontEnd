f74b8483f78101a0e919b370948fdb088f4b97d4
JhohanCortes <dicenadie2@gmail.com>
2023-07-06 23:36:02 -0500

    si

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import "./App.css";
-
-import { Route, Routes } from "react-router-dom";
-import Landing from "./Components/LandingPage/LandingPage";
-import Form from "./Components/Form/Register";
-import DetailCourses from "./Components/DetailCoursesProgramation/DetailCoursesProgramation";
-import SearchBar from "./Components/SearchBar/SearchBar";
-import Footer from "./Components/Footer/Footer.jsx";
-import CourseResults from "./Components/CourseResults/CourseResults";
-import CourseViewer from "./Components/CourseViewer/CourseViewer.jsx";
+import React from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Landing from './Components/LandingPage/LandingPage';
+import Form from './Components/Form/Register';
+import Login from './Components/Login/Login';
+import DetailCourses from './Components/DetailCoursesProgramation/DetailCoursesProgramation';
+import SearchBar from './Components/SearchBar/SearchBar';
+import Footer from './Components/Footer/Footer.jsx';
+import CourseResults from './Components/CourseResults/CourseResults';
+import CourseViewer from './Components/CourseViewer/CourseViewer.jsx';
+import NotFoundPage from './Components/NotFoundPage/NotFoundPage';
 
 function App() {
   return (
@@ -17,9 +19,10 @@ function App() {
         <Route path="/search" element={<CourseResults />} />
         <Route path="/view" element={<CourseViewer />} />
         <Route path="/detail" element={<DetailCourses />} />
-        <Route path="/login" element={<Form />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Form />} />
         <Route path="/" element={<Landing />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <Footer />
     </div>
