2f954241a9c4592155bb0dc832206863d5072175
Elián Rehbani <elianrehbani@MacBook-Air-de-Emmanuel.local>
2023-07-08 17:05:13 -0300

    cambios en el css

--- src/Components/LandingPage/LanguajeCourses/LenCourses.jsx
+++ src/Components/LandingPage/LanguajeCourses/LenCourses.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
+import { NavLink, useParams } from "react-router-dom";
 
 const LenCourse = () => {
     const [courses, setCourses] = useState([]);
@@ -20,12 +21,16 @@ const LenCourse = () => {
     return(
         <>
             {courses.map((course) => (
-                <div key={course._id} className='courseContainer'>
+                <div className='courseContainer flex flex-col justify-between border-2 hover:border-violet-600 transition-colors'>
                 <img src={course.image} alt="Imagen del curso" />
                 <h2>{course.title}</h2>
-                <p>{course.description}</p>
-                <p>{course.teacher}</p>
-                <a href={course.link} target="_blank" rel="noopener noreferrer">Ver Curso</a>
+                <p className="justify-center">{course.description}</p>
+                <p className="bg-gray-300 text-black w-40 mt-2 mx-auto rounded p-2">{course.teacher}</p>
+               { /* <a href={course.link} target="_blank" rel="noopener noreferrer">Ver Curso</a> */}
+                <div className="mt-auto">
+                <NavLink to={`/course/${course._id}`} className="m-3 w-2"><h1 className="relative text-white bg-purple-500 p-3 w-40 mt-0 mx-auto rounded-xl mb-0 customHoverShadow">Ver Curso</h1></NavLink>
+
+                </div>
                 </div>
             ))}
         </>
