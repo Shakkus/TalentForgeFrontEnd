7bd95e62667abd633670adc3ec7bcc7f7de3e4ed
Sergiofrfr <serfabian619@gmail.com>
2023-07-18 18:55:57 -0500

    feat/logica del rating

--- src/Components/CourseViewer/Rating.jsx
+++ src/Components/CourseViewer/Rating.jsx
@@ -1,21 +1,79 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaStar } from "react-icons/fa";
 import { IoMdCloseCircle } from "react-icons/io";
 import "./CourseViewer.css";
+import axios from "axios";
+import { useAuth } from "../../context/authContext";
+import { useParams } from "react-router";
 
 const Rating = () => {
 	const [rating, setRating] = useState(null);
 	const [hover, setHover] = useState(null);
+	const [showStars, setShowStars] = useState(false);
+	const { user } = useAuth();
+	const { id } = useParams();
 
-	const handleRatingChange = (value) => {
-		if (value === rating) {
-			// Si se hace clic en una estrella ya seleccionada, deseleccionarla
-			setRating(null);
-			console.log(rating);
-		} else {
-			console.log(rating);
-			setRating(value);
+	// VERIFICACION DE RATING
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.get(`rutabackend/${id}`);
+				const userRatingId = response.data.rating.userId;
+				const userRating = response.data.rating.rating;
+
+				if (userRatingId === userId) {
+					setShowStars(true);
+					setRating(userRating);
+				} else {
+					setShowStars(false);
+				}
+			} catch (error) {
+				console.log("Error al obtener los datos del curso:", error);
+			}
+		};
+
+		fetchData();
+	}, [user || localStorageUserId]);
+
+	// ----------------------
+
+	// INFORMACION DEL USUARIO
+	const [userId, setUserId] = useState("");
+	const localStorageUserId = localStorage.getItem("userId");
+	const googleUserId = user?.uid;
+
+	useEffect(() => {
+		if (localStorageUserId) {
+			setUserId(localStorageUserId);
+		} else if (googleUserId) {
+			setUserId(googleUserId);
 		}
+	}, [user || localStorageUserId]);
+
+	const userInfo = {
+		userId: userId,
+		rating: rating,
+	};
+
+	// -----------------------
+
+	const handleRatingSubmit = async () => {
+		// try {
+		// 	const response = await axios.put(`rutabackend/${id}`, userInfo);
+		// 	if (response.status === 200) {
+		// 		setShowStars(true);
+		// 	}
+
+		// 	if (response.status === 500) {
+		// 		setShowStars(false);
+		// 	}
+		// } catch (error) {
+		// 	setShowStars(false);
+		// }
+
+		setShowStars(!showStars);
+		console.log(userInfo.rating);
 	};
 
 	const [open, setOpen] = useState(false);
@@ -30,9 +88,32 @@ const Rating = () => {
 
 	return (
 		<div className="main-container">
-			<button className="rating-button" onClick={() => setOpen(!open)}>
-				RATE THIS VIDEO
-			</button>
+			{showStars ? (
+				<div className="fixed-rating">
+					{[...Array(5)].map((star, index) => {
+						const currentRating = index + 1;
+						return (
+							<label>
+								<input
+									className="radio"
+									type="radio"
+									name="rating"
+									value={currentRating}
+								/>
+								<FaStar
+									color={currentRating <= rating ? "#ffc107" : "#e7e7e7"}
+									className="stars"
+									size={40}
+								/>
+							</label>
+						);
+					})}
+				</div>
+			) : (
+				<button className="rating-button" onClick={() => setOpen(!open)}>
+					RATE THIS VIDEO
+				</button>
+			)}
 			{open && (
 				<div className="popup-container">
 					<div className="close-button-div">
@@ -50,7 +131,6 @@ const Rating = () => {
 											name="rating"
 											value={currentRating}
 											onClick={() => setRating(currentRating)}
-											onChange={() => handleRatingChange(currentRating)}
 										/>
 										<FaStar
 											color={
@@ -68,7 +148,9 @@ const Rating = () => {
 							})}
 						</div>
 					</div>
-					<button className="send-rating">SEND RATING</button>
+					<button className="send-rating" onClick={handleRatingSubmit}>
+						SEND RATING
+					</button>
 				</div>
 			)}
 		</div>
